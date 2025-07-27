ba998069f0c1126ff88d4ab8f299e7634788e6d9
にっしー <nissi1270561@gmail.com>
2025-07-27 22:56:23 +0000

    chore: セッション情報をsession_storeに保存するよう変更

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem "kamal", require: false
 gem "thruster", require: false
 gem "tailwindcss-rails", "~> 4.3.0"
 gem "puma"
+gem 'activerecord-session_store', "~> 2.2.0"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "groups/index"
-  get "groups/create"
-  get "groups/destroy"
+  root 'groups#index'
+  resources :groups, only: [:index, :create, :show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
