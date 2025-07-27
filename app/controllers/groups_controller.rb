469298ab2d668942d6ba985989ddb9da0ac76fdc
にっしー <nissi1270561@gmail.com>
2025-07-27 23:03:29 +0000

    feat: グループ管理画面の構築途中(あとでrebaseすること)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,31 @@
 class GroupsController < ApplicationController
   def index
+    @group = Group.new
   end
 
   def create
+
+  end
+
+  def show
+    # @group = Group.find()
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def destroy
   end
+
+  private
+    def group_create_params
+      params.expect(group: [:name, :participant_count])
+    end
+
+    def group_show_params
+      params.expect(group: [:id])
+    end
 end
