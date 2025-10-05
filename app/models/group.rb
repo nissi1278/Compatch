d537b681cf8ef63d36339ca37b759ce1b9c07762
にっしー <nissi1270561@gmail.com>
2025-10-05 12:21:06 +0000

    feature: groupモデルにparticipant_count仮想属性を追加し、バリデーションを設定。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
   has_many :participants, dependent: :destroy
+  attr_accessor :participant_count
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :participant_count, presence: true,
+            numericality: { only_integer: true, greater_than: 0 },
+            on: :create
 end
