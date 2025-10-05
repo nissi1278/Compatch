68719950cbb363c39c1843efed281ed5e37dddc9
にっしー <nissi1270561@gmail.com>
2025-10-05 12:21:42 +0000

    fix: groupインスタンス作成時のみ参加者人数に初期値を設定するよう変更

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,13 @@ class Group < ApplicationRecord
   validates :participant_count, presence: true,
             numericality: { only_integer: true, greater_than: 0 },
             on: :create
+
+  after_initialize :set_default_participant_count
+
+  private
+
+  # 画面初期表示時の参加者人数の初期値を設定
+  def set_default_participant_count
+    self.participant_count ||= 5
+  end
 end
