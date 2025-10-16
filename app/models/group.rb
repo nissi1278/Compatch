a3448ffc68e847166eb95b1af9c3da167dccf9c3
にっしー <nissi1270561@gmail.com>
2025-10-16 20:47:22 +0900

    refactor: バリデーションごとに改行するようコードを変更

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,11 @@ class Group < ApplicationRecord
   has_many :participants, dependent: :destroy
   attr_accessor :participant_count
 
-  validates :name, presence: true, length: { maximum: 20 }
-  validates :participant_count, presence: true,
+  validates :name, 
+            presence: true,
+            length: { maximum: 20 }
+  validates :participant_count,
+            presence: true,
             numericality: { only_integer: true, greater_than: 0 },
             on: :create
 
