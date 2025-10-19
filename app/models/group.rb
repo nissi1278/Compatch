69579ed04287e795a25e9c34a31ea550b65e154a
にっしー <nissi1270561@gmail.com>
2025-10-20 02:20:59 +0900

    refactor: 自分が作成したグループのみを取得するためのscopeをgroupモデルに追加。

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,6 +12,8 @@ class Group < ApplicationRecord
 
   after_initialize :set_default_participant_count
 
+  scope :created_by_session, ->(session_id) { where(session_id: session_id) }
+
   private
 
   # 画面初期表示時の参加者人数の初期値を設定
