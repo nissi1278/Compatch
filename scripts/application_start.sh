4c045aa28ebf2dec91b1abf2ec53797bffd16647
にっしー <nissi1270561@gmail.com>
2025-11-02 14:20:17 +0900

    chore: systemd用とrailsのコマンド実行用の環境変数ファイルを分離。

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,39 +1,45 @@
 export DEPLOY_DIR="/home/ec2-user/Compatch"
-export HOME="/home/ec2-user"
-export AWS_REGION="ap-northeast-1"
+ export HOME="/home/ec2-user"
+ export AWS_REGION="ap-northeast-1"
 
-chown -R ec2-user:ec2-user $DEPLOY_DIR
+ chown -R ec2-user:ec2-user $DEPLOY_DIR
 
-# systemdが読み込む環境変数ファイル用のディレクトリを作成
-mkdir -p /run/puma
-chown -R ec2-user:ec2-user /run/puma
-chmod 700 /run/puma
+ # systemdが読み込む環境変数ファイル用のディレクトリを作成
+ mkdir -p /run/puma
+ chown -R ec2-user:ec2-user /run/puma
+ chmod 700 /run/puma
 
-# SSMからマスターキーを取得し、ファイルに書き出す (rootで実行)
-aws ssm get-parameters \
+# SSMからマスターキーを取得し、「MASTER_KEY」に格納する (rootで実行)
+MASTER_KEY=$(aws ssm get-parameters \
   --name "/compatch/production/RAILS_MASTER_KEY" \
   --with-decryption \
   --query "Parameters[0].Value" \
-  --output text > /run/puma/environment
+  --output text)
 
-sed -i -e 's/^/export RAILS_MASTER_KEY=/' /run/puma/environment
+ # systemd 用のファイルを作成 (KEY=VALUE 形式)
+ echo "RAILS_MASTER_KEY=$MASTER_KEY" > /run/puma/environment_systemd
+ echo "RAILS_ENV=production" >> /run/puma/environment_systemd
 
-# ファイルの権限をec2-userのみに絞る
-chown ec2-user:ec2-user /run/puma/environment
-chmod 600 /run/puma/environment
+ # デプロイ作業用のファイルを作成 (export 形式)
+ echo "export RAILS_MASTER_KEY=$MASTER_KEY" > /run/puma/environment_deploy
+ echo "export RAILS_ENV=production" >> /run/puma/environment_deploy
 
-# --- 3. ec2-userとしてデプロイ作業を実行 ---
-su -l ec2-user -c "
-  source /run/puma/environment
+ chown ec2-user:ec2-user /run/puma/environment_systemd
+ chmod 600 /run/puma/environment_systemd
+ chown ec2-user:ec2-user /run/puma/environment_deploy
+ chmod 600 /run/puma/environment_deploy
 
-  cd $DEPLOY_DIR
+ # ec2-userとしてデプロイ作業を実行 ---
+ su -l ec2-user -c "
+   source /run/puma/environment_deploy
 
-  bundle install --without development test
+   cd $DEPLOY_DIR
 
-  RAILS_ENV=production bundle exec rails tailwind:build
-  RAILS_ENV=production bundle exec rails db:migrate
-  RAILS_ENV=production bundle exec rails assets:precompile
-"
+   bundle install --without development test
+   bundle exec rails tailwind:build
+   bundle exec rails db:migrate
+   bundle exec rails assets:precompile
+ "
 
-systemctl start puma.service
-systemctl restart nginx.service
+ systemctl start puma.service
+ systemctl restart nginx.service
