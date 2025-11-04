42b042aca5fd9dcb91b8c5405ca36c4c2f931f29
にっしー <nissi1270561@gmail.com>
2025-11-04 21:08:40 +0900

    feature: session管理はactive_recordではなくcookie_storeに変更。sessionsテーブルを使う必要がなかったため。

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :active_record_store, key: '_compatch_session', secure_session_only: true
+Rails.application.config.session_store :cookie_store, 
+key: '_compatch_session', expire_after: 20.years, secure: Rails.env.production? || Rails.env.staging?
