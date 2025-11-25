ab070e597b6312de6049af212895e91f50effcad
にっしー <nissi1270561@gmail.com>
2025-11-25 20:26:37 +0900

    chore: jsのstimuluse-useをimportmapでピン留め。

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus-use" # @0.52.3
