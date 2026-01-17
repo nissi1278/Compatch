33388477628790330bbe0ad5e00e68f403524d13
にっしー <nissi1270561@gmail.com>
2026-01-18 00:26:06 +0900

    chore: migrationを実行 ・usersテーブルを作成。主にユーザ情報とsession管理用のtokenを管理するテーブル。 ・user_identitiesテーブルを作成。主にユーザのOIDCの認証情報を管理するテーブル。 ・groupsテーブルからguest_tokenカラムを削除。session管理用のtokenはusersテーブル側で管理するため削除。

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_04_121426) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_17_152119) do
   create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "total_amount", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "share_token"
-    t.string "guest_token", null: false
-    t.index ["guest_token"], name: "index_groups_on_guest_token"
     t.index ["share_token"], name: "index_groups_on_share_token", unique: true
   end
 
@@ -32,5 +30,25 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_04_121426) do
     t.index ["group_id"], name: "index_participants_on_group_id"
   end
 
+  create_table "user_identities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider", null: false
+    t.string "sub", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "sub"], name: "index_user_identities_on_provider_and_sub", unique: true
+    t.index ["user_id"], name: "index_user_identities_on_user_id"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "auth_token", null: false
+    t.boolean "is_guest", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
+  end
+
   add_foreign_key "participants", "groups"
+  add_foreign_key "user_identities", "users"
 end
